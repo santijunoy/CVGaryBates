fda4a76d68630900459c4df0e890542d174b2c09
Santiago Junoy <santy_junoy@hotmail.com>
2023-02-20 14:10:44 -0300

    Primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,9 @@
+document.getElementById('boton_ocultar').addEventListener('click', function(){
+    console.log()
+    document.getElementById('div1').style.display = 'none' ;
+})
+
+document.getElementById('boton_mostrar').addEventListener('click', function(){
+    console.log()
+    document.getElementById('div1').style.display = '' ;
+})
